b523551a5c6efe3aebe484d2abd1a1eeacc8df4a
LenkaKukacka <48582265+LenkaKukacka@users.noreply.github.com>
2019-03-24 22:12:33 +0100

    Update ukol_basnicka.py

--- ukol_basnicka.py
+++ ukol_basnicka.py
@@ -1,6 +1,6 @@
 #9 otoceni basnicky
 with open("basnicka.txt" , encoding="utf-8") as soubor:
-    basnicka = list(soubor)#prevod na list nefunguje
+    basnicka = list(soubor)#prevod na list 
     basnicka.reverse()
 print(basnicka)
 
